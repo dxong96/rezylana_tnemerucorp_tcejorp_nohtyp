97e56b5945819213d7eadbf7b84ca6c0275e5932
5partace <36985194+5partace@users.noreply.github.com>
2018-09-30 20:09:21 +0800

    take data from dataholder and import dictionary directly from function3

--- function_8.py
+++ function_8.py
@@ -1,64 +1,18 @@
 import data_holder,csv,operator
+import function_3
 
-procurement1="C:/Users/bry/Downloads/Project Datasets/government-procurement/government-procurement-via-gebiz.csv"
-
-procurement_dictionary ={}
-total_procurement_dictionary={}
 max_procurement_dictionary={}
 sorted_max_procurement_dictionary_ascending={}
 sorted_max_procurement_dictionary_descending={}
 
-def procurement_dictionary1():
-    count=0
-    string_rep = "Data recorded below indicates the individual procurement amount for each agency:"
-    signify_end="------END OF CURRENT DATA------"
-    global procurement_dictionary
-    with open(procurement1,"r") as csvfile:
-        reader=csv.reader(csvfile)
-        #read allow me to call my line and specify the row
-        count =0
-        total_count=0
-        for line in reader:
-            if count > 0:
-                # this is so the first row which are all headers arent used
-                procurement_dictionary.setdefault(line[1],[]).append(round(float(line[6]),2))
-                #creating a dictionary where the values to a key is stored in a list, so for each similar string the value will be appended to that list.
-            else:
-                count +=1
-        for rows,value in procurement_dictionary.iteritems():
-            total_count +=1
-            string_rep = string_rep+"\n"+str(total_count) +") " + rows + ": " +str(value)
-            if total_count == len(procurement_dictionary):
-                string_rep = string_rep +"\n" + "%050s" %(signify_end)
-                return string_rep
-def total_procurement_dictionary1():
-    global total_procurement_dictionary
-    string_rep="Data recorded below indicates the total procurement amount for each agency:"
-    count=0
-    signify_end="------END OF CURRENT DATA------"
-    for k,v in procurement_dictionary.iteritems():
-        total = 0
-        # set total to 0 at this position so for each new category the total will start afresh at 0.
-        for number in v:
-            #finding total for each "category" and storing it into a new list
-            total = round(float(number),2) + total
-            #rounding to 2 decimal place
-            total_procurement_dictionary[k]=(round(total,2))
-    for rows,value in total_procurement_dictionary.iteritems():
-        count +=1
-        string_rep = string_rep +"\n" + str(count)+") " + rows + ": " +str(value)
-        if count == len(total_procurement_dictionary):
-            string_rep = string_rep +"\n" + "%050s" %(signify_end)
-            return string_rep
+
 def agency_max_procurement():
-    global procurement_dictionary
     global max_procurement_dictionary
     string_rep="Data recorded below indicates the maximum procurement amount for each agency:"
     count =0
     signify_end="------END OF CURRENT DATA------"
-    for k,v in procurement_dictionary.iteritems():
-        store_max=[]
-        combined_string= ",".join(str(number) for number in procurement_dictionary[k])
+    for k,v in function_3.procurement_dictionary.iteritems():
+        combined_string= ",".join(str(number) for number in function_3.procurement_dictionary[k])
         store_max=(combined_string).split(",")
         store_max=[round(float(elements),2) for elements in store_max]
         max_procurement_dictionary[k]=max(store_max)
@@ -79,7 +33,7 @@ def sorted_max_procurement(asc):
         sorted_max_procurement_dictionary_ascending=sorted(max_procurement_dictionary.items(), key=operator.itemgetter(1) ,reverse=False)
         for k,v in sorted_max_procurement_dictionary_ascending:
             total_count+=1
-            string_rep = string_rep +"\n" + str(total_count) +") "  +str(k) + ": " + str(v)
+            string_rep = string_rep + "\n" +str(total_count) +") "  +str(k) + ": " + str(v)
             if total_count == len(max_procurement_dictionary):
                 string_rep = string_rep +"\n" + "%050s" %(signify_end)
                 return string_rep
