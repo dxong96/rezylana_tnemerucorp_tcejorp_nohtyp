e21cc28f9167f381f7db95bce9116ef231cd81b6
yongyongjiejie <43488634+yongyongjiejie@users.noreply.github.com>
2018-09-30 12:41:03 +0800

    Update function_4.py

--- function_4.py
+++ function_4.py
@@ -2,7 +2,11 @@ import data_holder
 """
 Returns list of registered contractors that are awarded 
 """
-def get_registered_contractors():
+
+
+
+
+def get_registered_contractors_supplier():
     # at this point the cache is not yet set
     # so we initialize it
     registered_contractors = set()
@@ -15,16 +19,41 @@ def get_registered_contractors():
             continue
 
         for contractor in contractor_dict[procurement.supplier_name]:
-            registered_contractors.add(contractor)
+            if procurement.tender_detail_status == "Awarded to Suppliers":
+                registered_contractors.add(contractor)
+
+    return registered_contractors
+
+def get_registered_contractors_items():
+    # at this point the cache is not yet set
+    # so we initialize it
+    registered_contractors = set()
+    contractor_dict = data_holder.create_dict_for_list(data_holder.contractors, 'company_name')
+    for procurement in data_holder.procurements:
+        if not procurement.awarded:
+            continue
+
+        if procurement.supplier_name not in contractor_dict:
+            continue
+
+        for contractor in contractor_dict[procurement.supplier_name]:
+            if procurement.tender_detail_status == "Awarded by Items":
+                registered_contractors.add(contractor)
 
     return registered_contractors
 
 
 def function_4():
     result = []
-    result.append('Registered contractors:')
+    result.append('Registered contractors with supplier:')
 
-    contractor_names = map(lambda c: c.company_name, get_registered_contractors())
+    result2 = []
+    result2.append('\n' + '\n'+ '\n'+ 'Registered contractors with items:')
+
+    contractor_names = map(lambda c: c.company_name, get_registered_contractors_supplier())
     result.extend(contractor_names)
 
-    return '\n'.join(result)
+    contractor_names2 = map(lambda c: c.company_name, get_registered_contractors_items())
+    result2.extend(contractor_names2)
+
+    return '\n'.join(result + result2)
