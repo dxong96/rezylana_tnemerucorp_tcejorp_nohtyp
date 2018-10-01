45c49680e99cffd9743294c353bdf76b002399f8
5partace <36985194+5partace@users.noreply.github.com>
2018-10-01 14:00:58 +0800

    function 3 makes dictionary only if dictionary doesnt exist

--- function_3.py
+++ function_3.py
@@ -1,19 +1,19 @@
 import data_holder,csv,operator
 
-
 procurement_dictionary ={}
 total_procurement_dictionary={}
 
 def procurement_dictionary1():
     global procurement_dictionary
-    procurement_dictionary={}
+    if procurement_dictionary == {}:
+        for procurement in data_holder.procurements:
+            # this is so the first row which are all headers arent used
+            procurement_dictionary.setdefault(procurement.agency,[]).append(round(float(procurement.awarded_amt),2))
+            #creating a dictionary where the values to a key is stored in a list, so for each similar string the value will be appended to that list.
+
     string_rep = "Data recorded below indicates the individual procurement amount for each agency:"
-    signify_end="------END OF CURRENT DATA------"
-    total_count=0
-    for procurement in data_holder.procurements:
-        # this is so the first row which are all headers arent used
-        procurement_dictionary.setdefault(procurement.agency,[]).append(round(float(procurement.awarded_amt),2))
-        #creating a dictionary where the values to a key is stored in a list, so for each similar string the value will be appended to that list.
+    signify_end = "------END OF CURRENT DATA------"
+    total_count = 0
     for rows,value in procurement_dictionary.iteritems():
         total_count +=1
         string_rep = string_rep+"\n"+str(total_count) +") " + rows + ": " +str(value)
@@ -23,17 +23,19 @@ def procurement_dictionary1():
 
 def total_procurement_dictionary1():
     global total_procurement_dictionary
+    if total_procurement_dictionary == {}:
+        for k,v in procurement_dictionary.iteritems():
+            total = 0
+            # set total to 0 at this position so for each new category the total will start afresh at 0.
+            for number in v:
+                #finding total for each "category" and storing it into a new list
+                total = round(float(number),2) + total
+                #rounding to 2 decimal place
+                total_procurement_dictionary[k]=(round(total,2))
+
     string_rep="Data recorded below indicates the total procurement amount for each agency:"
     count=0
     signify_end="------END OF CURRENT DATA------"
-    for k,v in procurement_dictionary.iteritems():
-        total = 0
-        # set total to 0 at this position so for each new category the total will start afresh at 0.
-        for number in v:
-            #finding total for each "category" and storing it into a new list
-            total = round(float(number),2) + total
-            #rounding to 2 decimal place
-            total_procurement_dictionary[k]=(round(total,2))
     for rows,value in total_procurement_dictionary.iteritems():
         count +=1
         string_rep = string_rep +"\n" + str(count)+") " + rows + ": " +str(value)
