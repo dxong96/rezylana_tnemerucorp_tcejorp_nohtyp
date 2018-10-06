5c9bcc6ac48c2977c2fc2313bbf0227fbcfc6295
yongyongjiejie <43488634+yongyongjiejie@users.noreply.github.com>
2018-10-06 11:48:56 +0800

    Update function_4.py

--- function_4.py
+++ function_4.py
@@ -1,4 +1,5 @@
 import data_holder
+import sys
 """
 Returns list of registered contractors that are awarded 
 """
@@ -7,10 +8,12 @@ Returns list of registered contractors that are awarded
 
 
 def get_registered_contractors_supplier():
+    '''Get the list of contractors that is awarded to supplier'''
     # at this point the cache is not yet set
     # so we initialize it
     registered_contractors = set()
     contractor_dict = data_holder.create_dict_for_list(data_holder.contractors, 'company_name')
+    #loop through the csv file and check which contractor is a registered vendor and is awarded to suppliers
     for procurement in data_holder.procurements:
         if not procurement.awarded:
             continue
@@ -24,11 +27,14 @@ def get_registered_contractors_supplier():
 
     return registered_contractors
 
+
 def get_registered_contractors_items():
+    '''Get the list of contractors that is awarded by items'''
     # at this point the cache is not yet set
     # so we initialize it
     registered_contractors = set()
     contractor_dict = data_holder.create_dict_for_list(data_holder.contractors, 'company_name')
+    # loop through the csv file and check which contractor is a registered vendor and is awarded by items
     for procurement in data_holder.procurements:
         if not procurement.awarded:
             continue
@@ -42,18 +48,20 @@ def get_registered_contractors_items():
 
     return registered_contractors
 
-
 def function_4():
-    result = []
-    result.append('Registered contractors with supplier:')
+    ''' append the list of registered vendor awarded and awarded by items and return the result '''
+    supplierResult = []
+    supplierResult.append('Awarded Registered contractors:' + '\n')
 
-    result2 = []
-    result2.append('\n' + '\n'+ '\n'+ 'Registered contractors with items:')
+    itemResult = []
+    itemResult.append('\n'+ '\n'+ 'Registered contractors awarded with items:' + '\n')
 
-    contractor_names = map(lambda c: c.company_name, get_registered_contractors_supplier())
-    result.extend(contractor_names)
+    awarded_names = map(lambda c: c.company_name, get_registered_contractors_supplier())
+    supplierResult.extend(awarded_names)
+    sortSupplier = supplierResult[:1] + sorted(supplierResult[1:])
 
-    contractor_names2 = map(lambda c: c.company_name, get_registered_contractors_items())
-    result2.extend(contractor_names2)
+    awardeditems_names = map(lambda c: c.company_name, get_registered_contractors_items())
+    itemResult.extend(awardeditems_names)
+    sortItem = itemResult[:1] + sorted(itemResult[1:])
+    return '\n'.join(sortSupplier+ sortItem)
 
-    return '\n'.join(result + result2)
